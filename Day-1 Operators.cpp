85414dfc86f8dbfaee3b553372fe072dbf23487b
Anna1008 <72347834+Anna1008@users.noreply.github.com>
2020-10-05 18:09:06 +0530

    Learned Operators

---
+++ Day-1 Operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout <<"enter a number a:";
+	cin>>a;
+	cout <<"enter a number b:";
+    cin>>b;
+	cout << ++a;
+	cout << b++;
+	return 0;
+}
