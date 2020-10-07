64010b25bb14a9dbc4cecaee98de37a8f143affe
Akshay Nille <72347834+Anna1008@users.noreply.github.com>
2020-10-07 11:40:24 +0530

    Learned about Math Library
    Today, I learned how to do mathematical calculation by using Math Library..

---
+++ Day-3 Math library.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+	int a = 2;
+	cout << "sin = "<<sin(a);
+	cout << "\ncos = " <<cos(a);
+	cout << "\ntan = " <<tan(a);
+	cout << "\nsquare root = " <<sqrt(a);
+	return 0;
+}
+	
