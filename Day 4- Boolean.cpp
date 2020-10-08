12ca0a3b9b5c337c097b8a83dfdb255aaaea73be
Akshay Nille <72347834+Anna1008@users.noreply.github.com>
2020-10-08 08:54:59 +0530

    Learned Implementation of Boolean function..

---
+++ Day 4- Boolean.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	bool ACESChallengeIsGoing = true;
+	cout <<"Since ACES Challenge is started, its value is: "<< ACESChallengeIsGoing<<"\n";
+	bool ACESChallengeCompleted = false;
+	cout <<"ACES Challenge is on going now..."<<"So its value is: "<< ACESChallengeCompleted<<"\n";
+	return 0;
+}
