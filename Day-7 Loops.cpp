7206ec758a5ac4f8ec4ae77b5361c5d7dd5895f7
Akshay Nille <72347834+Anna1008@users.noreply.github.com>
2020-10-11 19:41:33 +0530

    Learned Loops..

---
+++ Day-7 Loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	for(int i=0; i<5; i++){   //For Loop
+		cout<<i<<" ";
+	
+	}
+	
+	cout<<"\n""*******************""\n";
+	
+	int i =10;
+	while(i<25){     //While Loop
+		cout<<i<<" ";
+		i = i+1;
+	}
+	
+	return 0;
+}
