8d2a79c4f3bb38f8fa275b6f9180663f0d00a6d3
Akshay Nille <72347834+Anna1008@users.noreply.github.com>
2020-10-14 10:49:15 +0530

    Learned Functions..

---
+++ Day-10 Functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int add(int x, int y){    //Function 
+	return x+y;
+}
+double add(double x, double y){  //Function Overloading
+	return x+y;
+}
+int main(){
+	cout<<"Add int :"<<add(25,75)<<"\n";
+	cout<<"Add double :"<<add(15.6,20.5);
+	return 0;
+}
