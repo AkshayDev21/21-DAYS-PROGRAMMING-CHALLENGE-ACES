341dbe0361b0e3939cb9ae5b5401211632304a9c
Akshay Nille <72347834+Anna1008@users.noreply.github.com>
2020-10-15 11:13:26 +0530

    Learned Class..

---
+++ Day-11 Class.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+#include <iostream>
+class Student{
+	public:
+		int id;
+		string name;
+		
+	void put_details(int i, string n){
+		id = i;
+		name = n;
+	}
+		
+	
+		
+	void get_details(){
+		cout<<"id :"<<id;
+		cout<<"\nname :"<<name;
+	}
+};
+
+int main(){
+	Student s1;
+	
+	int s_id;
+	string s_name;
+	
+	cout<<"Enter Id of Student :";
+	cin>>s_id;
+	
+	cout<<"Enter name of Student :";
+	cin>>s_name;
+	
+	s1.put_details(s_id, s_name);
+	s1.get_details();
+}
